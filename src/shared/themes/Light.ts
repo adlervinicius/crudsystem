321e377aeaea47d3ece1a13a62a8f643802ae526
Adler <adler.vinicius22@gmail.com>
2023-10-23 13:55:36 -0300

    feat: adicionado o tema claro e o provider

---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material";
+import { blue, cyan } from "@mui/material/colors";
+
+export const LightTheme = createTheme({
+    palette: {
+        primary: {
+            main: blue[700],
+            dark: blue[800],
+            light: blue[500],
+            contrastText: '#ffff',
+        },
+        secondary: {
+            main: cyan[500],
+            dark: cyan[400],
+            light: cyan[300],
+            contrastText: '#ffff',
+        },
+        background: {
+            default: '#ffff',
+            paper: '#f7f6f3',
+        }
+    }
+});
