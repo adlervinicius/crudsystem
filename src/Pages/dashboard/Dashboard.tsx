e8af878a0b1dcb93a6e6feb63aa1659d17f62e60
Adler <adler.vinicius22@gmail.com>
2023-10-24 10:48:21 -0300

    feat: responsividade do header

--- src/Pages/dashboard/Dashboard.tsx
+++ src/Pages/dashboard/Dashboard.tsx
@@ -4,7 +4,7 @@ import { LayoutBasePagina } from '../../shared/layouts/LayoutBasePagina';
 export const Dashboard = () => {
 
     return (
-        <LayoutBasePagina titulo='Página inicial'>
+        <LayoutBasePagina titulo='Página inicial' barraDeFerramentas={<>Barra de Ferramentas</>}>
         </LayoutBasePagina>
     );
 };
--- src/shared/layouts/LayoutBasePagina.tsx
+++ src/shared/layouts/LayoutBasePagina.tsx
@@ -1,15 +1,19 @@
+import { ReactNode } from 'react';
 import { Box, Icon, IconButton, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { useAppDrawerContext } from '../contexts';
+
 
 interface ILayoutBasePaginaProps {
     children: React.ReactNode;
     titulo: string;
+    barraDeFerramentas?: ReactNode;
 }
 
-export const LayoutBasePagina: React.FC<ILayoutBasePaginaProps> = ({ children, titulo }) => {
+export const LayoutBasePagina: React.FC<ILayoutBasePaginaProps> = ({ children, titulo, barraDeFerramentas }) => {
 
     const theme = useTheme();
     const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+    const mdDown = useMediaQuery(theme.breakpoints.down('md'));
 
     const { toggleDrawerOpen } = useAppDrawerContext();
 
@@ -23,7 +27,7 @@ export const LayoutBasePagina: React.FC<ILayoutBasePaginaProps> = ({ children, t
         >
             <Box 
                 padding={1} 
-                height={theme.spacing(12)}
+                height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)}
                 display='flex'
                 alignItems='center'
                 gap={1}
@@ -34,16 +38,23 @@ export const LayoutBasePagina: React.FC<ILayoutBasePaginaProps> = ({ children, t
                     </IconButton>
                 )}
 
-                <Typography variant='h5'>
+                <Typography 
+                    variant={smDown ? 'h5' : mdDown ? 'h4' : 'h3'}
+                    whiteSpace='nowrap'
+                    overflow='hidden'
+                    textOverflow='ellipsis'
+                >
                     {titulo}
                 </Typography>
             </Box>
 
-            <Box>
-                Barra de ferramentas
-            </Box>
+            {barraDeFerramentas && (
+                <Box>
+                    {barraDeFerramentas}
+                </Box>
+            )}
 
-            <Box>
+            <Box flex={1} overflow='auto'>
                 {children}
             </Box>
         </Box>
