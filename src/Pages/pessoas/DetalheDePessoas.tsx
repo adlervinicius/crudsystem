5ca7844c6ba980fe6cd4d39d6c5900afa6cc13e6
Adler <adler.vinicius22@gmail.com>
2023-10-27 12:13:32 -0300

    fix: criação da responsividade e layout da parte de cadastro e editar usuario

--- src/Pages/pessoas/DetalheDePessoas.tsx
+++ src/Pages/pessoas/DetalheDePessoas.tsx
@@ -5,7 +5,7 @@ import { FerramentasDeDetalhe } from '../../shared/components';
 import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
 import { Form } from '@unform/web';
 import { VTextField } from '../../shared/forms';
-import { LinearProgress } from '@mui/material';
+import { Box, Grid, LinearProgress, Paper, Typography } from '@mui/material';
 import { FormHandles } from '@unform/core';
 
 interface IFormData {
@@ -95,14 +95,57 @@ export const DetalheDePessoas: React.FC = () => {
                 />
             }
         >
-            {isLoading && (
-                <LinearProgress variant='indeterminate' />
-            )}
 
             <Form ref={formRef} onSubmit={handleSave}>
-                <VTextField placeholder='Nome completo' name='nomeCompleto' />
-                <VTextField placeholder='Email' name='email' />
-                <VTextField placeholder='Cidade' name='cidadeId' />
+                <Box 
+                    margin={1} 
+                    display='flex' 
+                    flexDirection='column' 
+                    component={Paper}
+                    variant='outlined'
+                >
+                    <Grid container direction='column' padding={2} spacing={2}>
+                        {isLoading && (
+                            <Grid item>
+                                <LinearProgress variant='indeterminate' />
+                            </Grid>
+                        )}
+                        <Grid item>
+                            <Typography variant='h6'>Geral</Typography>
+                        </Grid>
+                        <Grid container item direction='row' spacing={2}>
+                            <Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
+                                <VTextField
+                                    fullWidth
+                                    label='Nome completo' 
+                                    name='nomeCompleto'
+                                    disabled={isLoading}
+                                    onChange={e => setNome(e.target.value)}
+                                />
+                            </Grid>
+                        </Grid>
+                        <Grid container item direction='row' spacing={2}>
+                            <Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
+                                <VTextField
+                                    fullWidth
+                                    label='Email'
+                                    name='email'
+                                    disabled={isLoading}
+                                />
+                            </Grid>
+                        </Grid>
+                        <Grid container item direction='row' spacing={2}>
+                            <Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
+                                <VTextField
+                                    fullWidth
+                                    label='Cidade'
+                                    name='cidadeId'
+                                    disabled={isLoading}
+                                />
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Box>
             </Form>
         </LayoutBasePagina>
     );
--- src/shared/forms/VTextField.tsx
+++ src/shared/forms/VTextField.tsx
@@ -33,10 +33,10 @@ export const VTextField: React.FC<TVTextFieldProps> = ({ name, ...rest }) => {
             helperText={error}
             defaultValue={defaultValue}
 
-            onKeyDown={() => error ? clearError() : undefined}
+            onKeyDown={(e) => { error && clearError(); rest.onKeyDown?.(e);}}
 
             value={value}
-            onChange={e => setValue(e.target.value)}
+            onChange={e => {setValue(e.target.value); rest.onChange?.(e);}}
         />
     );
 };
