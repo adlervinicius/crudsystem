2804ead778b6f59feef3253bd9060891b3b4d18a
Adler <adler.vinicius22@gmail.com>
2023-11-01 10:40:30 -0300

    feat: ajuste tela de login, e ajustes no localstorage

--- src/shared/contexts/AuthContext.tsx
+++ src/shared/contexts/AuthContext.tsx
@@ -18,36 +18,36 @@ interface IAuthProviderProps {
 
 export const AuthProvider: React.FC<IAuthProviderProps> = ({ children }) => {
 
-    const [accessToken, setAccessToken] = useState<string>();
-
+    const [iaccessToken, setIAccessToken] = useState<string>();
+    console.log(LOCAL_STORAGE_KEY_ACCESS_TOKEN);
     useEffect(() => {
-        const accessToken = localStorage.getItem('APP_ACCESS_TOKEN');
+        const accessToken = localStorage.getItem(LOCAL_STORAGE_KEY_ACCESS_TOKEN);
 
         if (accessToken) {
-            // setAccessToken(JSON.parse(accessToken));
+            setIAccessToken(JSON.parse(accessToken));
         } else {
-            setAccessToken(undefined);
+            setIAccessToken(undefined);
         }
     }, []);
 
-    const handleLogin = useCallback(async(email: string, password: string) => {
+    const handleLogin = useCallback(async (email: string, password: string) => {
         const result = await AuthService.auth(email, password);
 
         if(result instanceof Error) {
             return result.message;
         } else {
             localStorage.setItem(LOCAL_STORAGE_KEY_ACCESS_TOKEN, JSON.stringify(result.accessToken));
-            setAccessToken(result.accessToken);
+            setIAccessToken(result.accessToken);
         }
 
     }, []);
 
     const handleLogout = useCallback(() => {
         localStorage.removeItem(LOCAL_STORAGE_KEY_ACCESS_TOKEN);
-        setAccessToken(undefined);
+        setIAccessToken(undefined);
     }, []);
 
-    const isAuthenticated = useMemo(() => !!accessToken, [accessToken]);
+    const isAuthenticated = useMemo(() => iaccessToken !== undefined, [iaccessToken]);
     
     return (
         <AuthContext.Provider value={{ isAuthenticated, login: handleLogin, logout: handleLogout }}>
